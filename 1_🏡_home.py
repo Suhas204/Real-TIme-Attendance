353aec8e8595e468816d0cfc91891aec116768d9
Suhas204 <106133660+Suhas204@users.noreply.github.com>
2024-04-13 12:26:27 +0530

    Add files via upload

---
+++ 1_🏡_home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title= "Computer-Vision Based Attendance",
+    page_icon= "🎥",
+)
+
+st.title("Real-Time Attendance Detection")
+st.sidebar.success("Select a page above")
